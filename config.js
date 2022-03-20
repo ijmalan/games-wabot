09bb7366524900edb61e0156d790f311d174c740
ijmalan <64469598+ijmalan@users.noreply.github.com>
2022-03-21 01:04:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
+global.linkGC = ['']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
   '6289673766582': { // put your number here
